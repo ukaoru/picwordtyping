0de630830913e3eadbc489a1dcd823241f9e1df1
uchikaoru <uchikaoru@gmail.com>
2023-05-15 11:21:25 +0900

    rev

--- makeProchQR.py
+++ makeProchQR.py
@@ -1,4 +1,4 @@
-# Time-stamp: <2023-05-14 19:16:54 uchik>
+# Time-stamp: <2023-05-15 11:20:55 uchik>
 
 #!/usr/bin/env python
 # coding: utf-8
@@ -15,8 +15,8 @@ from PIL import Image
 def inpstr():
     if input := st.session_state.txt:
         pic = '_tmpQR.png'
-        prstr = dt.datetime.now().strftime('%c')
-        msg = f'{input} @ プロチャレ'
+        #prstr = dt.datetime.now().strftime('%c')
+        msg = f'{input} @ プロチャレ2023'
         print(msg)
         qrcode.make(msg).save(pic)
         img = Image.open(pic)
@@ -31,7 +31,9 @@ def inpstr():
 if __name__ == "__main__":
     # executed only once at the beginning
     if not 'txt' in st.session_state:
-        st.text_input('名前+読み仮名 (例「神谷王子 かみやおうこ」) 「Enter」',
+        st.text('「V+V番号+名前+読み仮名」を入力して「Enter」')
+        st.text('　　　(V番号は、スタッフ予定表の一番左の列)')
+        st.text_input('(例「V05 神谷王子 かみやおうこ」) ',
                       '', key='txt', on_change=inpstr)
         #st.button("Submit", on_click=inpstr)
 

d3019146c6937056d642090107fc6be05abc1b93
uchikaoru <uchikaoru@gmail.com>
2023-05-28 19:45:41 +0900

    add again

--- addcalcgame.py
+++ addcalcgame.py
@@ -1,4 +1,4 @@
-# Time-stamp: <2023-05-26 14:37:03 uchik>
+# Time-stamp: <2023-05-28 19:44:58 uchik>
 
 #!/usr/bin/env python
 # coding: utf-8
@@ -42,10 +42,10 @@ def giveprob():
     #uplim = (10**st.session_state.level)//2 - 1
     x = st.session_state.x = random.randint(1, uplim)
     y = st.session_state.y = random.randint(1, uplim)
-    #st.session_state.ans = x + y
-    st.session_state.ans = x - y
-    msg = f'What is {x} - {y} ?'
-    msg2 = f'What is {x} minus {y} ?'
+    st.session_state.ans = x + y
+    #st.session_state.ans = x - y
+    #msg2 = f'What is {x} minus {y} ?'
+    msg2 = msg = f'What is {x} + {y} ?'
     speaktext(msg2)
     st.text_input(msg, '', key='txt', on_change=checkans)
     st.write(f'Score: {st.session_state.point} / {st.session_state.idx}')
@@ -57,14 +57,14 @@ def checkans():
     x, y = st.session_state.x, st.session_state.y 
     if inp := st.session_state.txt:
         if inp.replace('-', '').isdigit() and int(inp) == ans:
-            msg = f'Correct! {x} - {y} = {ans}.'
-            msg2 = f'Correct! {x} minus {y} = {ans}.'
+            #msg2 = f'Correct! {x} minus {y} = {ans}.'
+            msg2 = msg = f'Correct! {x} + {y} = {ans}.'
             st.write(msg)
             speaktext(msg2)
             st.session_state.point += 1
         else:
-            msg = f'Wrong.  {x} - {y} = {ans}, not {inp}.'
-            msg2 = f'Wrong.  {x} minus {y} = {ans}, not {inp}.'
+            #msg2 = f'Wrong.  {x} minus {y} = {ans}, not {inp}.'
+            msg2 = msg = f'Wrong.  {x} + {y} = {ans}, not {inp}.'
             st.write(msg)
             speaktext(msg2)
         st.session_state.txt  = ''  # to clear text_input box
